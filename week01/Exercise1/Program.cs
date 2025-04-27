30bedc1a684c2bfeb650df1128062a900552408e
Varaprasad Chilumula <varaprasadchilumula@gmail.com>
2025-04-27 11:21:10 +0530

    commit Finished Prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {last}, {first} {last}");
     }
 }
